a4d35c4860568129a3db21a24208afa7dfe692b7
ChuangLan <ChuangLan@ChuangLan-PC>
2017-09-20 13:46:38 +0800

    配置文件调整

--- user-provider-service/src/main/java/cn/UserProviderServiceApp.java
+++ user-provider-service/src/main/java/cn/UserProviderServiceApp.java
@@ -3,6 +3,8 @@ package cn;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -12,10 +14,16 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient     // Eureka Client 标识
 @SpringBootApplication  // Spring Boot 应用标识
 @MapperScan("cn.dao") //mapper 接口类扫描包配置
-public class UserProviderServiceApp 
+public class UserProviderServiceApp extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
     	SpringApplication.run(UserProviderServiceApp.class, args);
     }
+    
+    @Override  
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {  
+        builder.sources(this.getClass());  
+        return super.configure(builder);  
+    }  
 }
