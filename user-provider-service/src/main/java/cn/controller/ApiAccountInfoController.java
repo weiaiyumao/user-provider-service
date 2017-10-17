df2fdf6e6a21e9093a2b704a1ee10d5fe751ffbe
empty <weiaiyumao@163.com>
2017-10-17 10:43:46 +0800

    api接口对外Controller

--- user-provider-service/src/main/java/cn/controller/ApiAccountInfoController.java
+++ user-provider-service/src/main/java/cn/controller/ApiAccountInfoController.java
@@ -1,14 +1,14 @@
 package cn.controller;
 
-import java.util.Calendar;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import cn.utils.DateUtils;
+import main.java.cn.common.BackResult;
+import main.java.cn.common.ResultCode;
+import main.java.cn.domain.ApiAccountInfoDomain;
 import main.java.cn.service.ApiAccountInfoBusService;
 
 @RestController
@@ -19,37 +19,38 @@ public class ApiAccountInfoController {
 	@Autowired
 	private ApiAccountInfoBusService apiAccountInfoBusService;
 	
-//	@RequestMapping("/save")
-//	public BackResult<Boolean> save(ApiAccountInfoDomain info) {
-//		
-//		BackResult<Boolean> result = new BackResult<Boolean>();
-//		try {
-//			
-//			int save(ApiAccountInfo info);
-//			
-//			trdOrderService.payCallBack(outTrdOrder, orderStatus,traOrder);
-//			result.setResultObj(Boolean.TRUE);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} 
-//		
-//		return result;
-//	}
-	
-	public static void main(String[] args) {
-		Calendar calendar = Calendar.getInstance();
-		int year = 2017;
-		int month = 0;
-		int date = 1;
-		calendar.set(year, month, date);
-		int maxDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-		int minDay = calendar.getActualMinimum(Calendar.DAY_OF_MONTH);
+	@RequestMapping("/findByCreUserId")
+	public BackResult<ApiAccountInfoDomain> save(String creUserId) {
 		
-		for (int i = minDay; i <= maxDay; i++) {
-			calendar.set(year, month, i);
-
-			logger.info("开始执行" + DateUtils.formatDate(calendar.getTime(), "yyyy-MM-dd") + "的数据");
-			
-		}
+		BackResult<ApiAccountInfoDomain> result = new BackResult<ApiAccountInfoDomain>();
+		
+		try {
+			result = apiAccountInfoBusService.findByCreUserId(creUserId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("用户ID：" + creUserId + "查询api账户信息出现系统异常：" + e.getMessage());
+			result.setResultCode(ResultCode.RESULT_FAILED);
+			result.setResultMsg("系统异常");
+		} 
+		
+		return result;
 	}
+	
+	@RequestMapping("/updateApiAccountInfo")
+	public BackResult<ApiAccountInfoDomain> save(ApiAccountInfoDomain domain) {
+		
+		BackResult<ApiAccountInfoDomain> result = new BackResult<ApiAccountInfoDomain>();
+		
+		try {
+			result = apiAccountInfoBusService.updateApiAccountInfo(domain);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("用户ID：" + domain.getCreUserId() + "修改api账户信息出现系统异常：" + e.getMessage());
+			result.setResultCode(ResultCode.RESULT_FAILED);
+			result.setResultMsg("系统异常");
+		} 
+		
+		return result;
+	}
+
 }
